cae3fde232fb2ef30f4273f9bf3321f7a9e839c1
karishma singh <karishma1294#gmail.com>
2017-12-17 00:39:36 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app= express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+app.use(express.static(__dirname+'/public'));
+
+
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log',log+ '\n',(err)=>{
+    if(err){
+      console.log('Unable to write in the log file');
+    }
+  });
+next();
+
+});
+
+// app.use((req,res,next) =>{
+//   res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+})
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to my website'
+    });
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();''
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle: 'About Page'
+  });
+})
+
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    status:'404',
+    errorMessage :'Unable to load the page'
+      });
+})
+
+app.listen(4200,()=>{
+console.log('server is up on 4200');
+});
